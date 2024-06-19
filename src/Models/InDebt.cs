a130f0b819bf21d6b1b527521e844f123ff621d7
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-06-19 09:51:38 -0300

    Added initial "InDebt" class structure

---
+++ src/Models/InDebt.cs
@@ -0,0 +1,20 @@
+using System.Component.DataAnnotations;
+
+namespace InterProject.Models;
+
+/*
+ * O que uma pessoa endividada tem?
+ * Ela tem:
+ * - Um ID
+ * - Um nome
+ * - Um CPF
+ * - Um email
+ * - O valor da dívida
+*/
+
+public class InDebt {
+	private int id;
+
+	public InDebt() {}
+
+}
