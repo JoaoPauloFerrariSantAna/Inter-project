8df03d38743ac78747b724c9eb65de6ccc1bc158
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-06-20 10:45:22 -0300

    Moved client data to separated class (ClientModel)

--- src/Models/IndebtModel.cs
+++ src/Models/IndebtModel.cs
@@ -5,31 +5,17 @@ namespace InterProject.Models;
 /*
  * O que uma pessoa endividada tem?
  * Ela tem:
- * - Um ID
- * - Um nome
- * - Um CPF
- * - Um email
  * - Uma data de nascimento
  * - O valor da dívida
 */
 
-public class InDebtModel {
-	public string name			{ get; set; } = string.Empty;
-	public DateTime? dataNasc	{ get; set; }
-	// XXX.XXX-YY
-	private string cpf			{ get; set; } = string.Empty;
-	private string email		{ get; set; } = string.Empty;
-	private DateTime initialDate { get; set; }	// Timestamp not null
-	private DateTime endDate { get; set; }		// Timestamp not null
+public class IndebtModel {
+	private int indebtId { get; set; }
+	public double debtToPay { get; set; }
 
 	// Constructor
-	public InDebtModel(string name, DateTime dataNasc, string cpf, string email,
-						DateTime initialDate, DateTime endDate) {
-		this.name = name;
-		this.dataNasc = dataNasc;
-		this.cpf = cpf;
-		this.email = email;
-		this.initialDate = initialDate;
-		this.endDate = endDate;
+	public IndebtModel(int indebtId, double debtToPay) {
+		this.indebtId = indebtId;
+		this.debtToPay = debtToPay;
 	}
 }
